da99d202d841ef58fbd35d4e27dbb68c251f3d9f
Anthony Agbator <tonjoe99@gmail.com>
2019-11-06 18:01:52 +0100

    Updates

---
+++ precache-manifest.7f056970ae4ec3e789424789309c93ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d4dba94753d58597ff513273f2ef2d",
+    "url": "/omdb/index.html"
+  },
+  {
+    "revision": "d3a277741672a41dccdb",
+    "url": "/omdb/static/css/main.be8a3366.chunk.css"
+  },
+  {
+    "revision": "34e5faa1fdee8b2d7c82",
+    "url": "/omdb/static/js/2.5335d7c2.chunk.js"
+  },
+  {
+    "revision": "d3a277741672a41dccdb",
+    "url": "/omdb/static/js/main.184fd02f.chunk.js"
+  },
+  {
+    "revision": "e680e51120b20023b359",
+    "url": "/omdb/static/js/runtime-main.c5ff212a.js"
+  }
+]);
